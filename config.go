5a73a055bf701f2739d1309422bc741791531f78
1Satan3 <mega.itachi.uchiha@mail.ru>
2020-12-12 02:07:13 +0300

    Update config files

--- config.go
+++ config.go
@@ -1,7 +1,10 @@
 package main
 
 type Config struct {
-	DatabaseURL string `toml:"database_url"`
+	DatabaseURL          string `toml:"database_url"`
+	GoroutinesMultiplier int    `toml:"goroutines_multiplier"`
+	SendTo               string `toml:"database"`
+	TelegramBotKey       string `toml:"telegram_bot_key"`
 }
 
 func NewConfig() *Config {
